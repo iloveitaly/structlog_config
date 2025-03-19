4f81268411e320a3fbc885365444895c5dd51d49
Michael Bianco <mike@mikebian.co>
2025-03-19 12:32:18 -0600

    refactor: extract stdlib logging functions to new module
    - Moved standard library logging functions to a dedicated `stdlib_logging` module for better organization and code separation.
    - This improves code maintainability and readability by separating concerns within the logging configuration.
    
    Generated-by: aiautocommit

--- structlog_config/__init__.py
+++ structlog_config/__init__.py
@@ -33,6 +33,12 @@ from structlog.typing import EventDict, ExcInfo
 from typeid import TypeID
 
 from .environments import is_production, is_pytest, is_staging
+from .stdlib_logging import (
+    _get_log_level,
+    redirect_stdlib_loggers,
+    reset_stdlib_logger,
+    silence_loud_loggers,
+)
 from .warnings import redirect_showwarnings
 
 logging.basicConfig(
@@ -181,11 +187,6 @@ PROCESSORS: list[structlog.types.Processor] = [
 ]
 
 
-def _get_log_level():
-    log_level = config("LOG_LEVEL", default="INFO", cast=str)
-    return logging.getLevelNamesMapping()[log_level.upper()]
-
-
 def _logger_factory():
     """
     Allow dev users to redirect logs to a file using PYTHON_LOG_PATH
@@ -204,108 +205,6 @@ def _logger_factory():
 
     else:
         return structlog.PrintLoggerFactory()
-
-
-def reset_stdlib_logger(
-    logger_name: str, default_structlog_handler, level_override=None
-):
-    std_logger = logging.getLogger(logger_name)
-    std_logger.propagate = False
-    std_logger.handlers = []
-    std_logger.addHandler(default_structlog_handler)
-
-    if level_override:
-        std_logger.setLevel(level_override)
-
-
-def redirect_stdlib_loggers():
-    """
-    Redirect all standard logging module loggers to use the structlog configuration.
-
-    Inspired by: https://gist.github.com/nymous/f138c7f06062b7c43c060bf03759c29e
-    """
-    from structlog.stdlib import ProcessorFormatter
-
-    level = _get_log_level()
-
-    # Create a handler for the root logger
-    handler = logging.StreamHandler(sys.stdout)
-    handler.setLevel(level)
-
-    # TODO I don't understand why we can't use a processor stack as-is here. Need to investigate further.
-
-    # Use ProcessorFormatter to format log records using structlog processors
-    formatter = ProcessorFormatter(
-        processors=[
-            # required to strip extra keys that the structlog stdlib bindings add in
-            structlog.stdlib.ProcessorFormatter.remove_processors_meta,
-            PROCESSORS[-1]
-            if not is_production() and not is_staging()
-            # don't use ORJSON here, as the stdlib formatter chain expects a str not a bytes
-            else structlog.processors.JSONRenderer(sort_keys=True),
-        ],
-        # processors unique to stdlib logging
-        foreign_pre_chain=[
-            # logger names are not supported when not using structlog.stdlib.LoggerFactory
-            # https://github.com/hynek/structlog/issues/254
-            structlog.stdlib.add_logger_name,
-            # omit the renderer so we can implement our own
-            *PROCESSORS[:-1],
-        ],
-    )
-    handler.setFormatter(formatter)
-
-    # Configure the root logger
-    root_logger = logging.getLogger()
-    root_logger.setLevel(level)
-    root_logger.handlers = [handler]  # Replace existing handlers with our handler
-
-    # Disable propagation to avoid duplicate logs
-    root_logger.propagate = True
-
-    # TODO there is a JSON-like format that can be used to configure loggers instead :/
-    std_logging_configuration = {
-        "httpcore": {},
-        "httpx": {
-            "levels": {
-                "INFO": "WARNING",
-            }
-        },
-        "azure.core.pipeline.policies.http_logging_policy": {
-            "levels": {
-                "INFO": "WARNING",
-            }
-        },
-    }
-    """
-    These loggers either:
-
-    1. Are way too chatty by default
-    2. Setup before our logging is initialized
-
-    This configuration allows us to easily override various loggers as we add additional complexity to the application
-    """
-
-    # now, let's handle some loggers that are probably already initialized with a handler
-    for logger_name, logger_config in std_logging_configuration.items():
-        reset_stdlib_logger(
-            logger_name,
-            handler,
-            logger_config.get("levels", {}).get(logging.getLevelName(level)),
-        )
-
-    # TODO do i need to setup exception overrides as well?
-    # https://gist.github.com/nymous/f138c7f06062b7c43c060bf03759c29e#file-custom_logging-py-L114-L128
-    if sys.excepthook != sys.__excepthook__:
-        logging.getLogger(__name__).warning("sys.excepthook has been overridden.")
-
-
-def silence_loud_loggers():
-    # unless we are explicitly debugging asyncio, I don't want to hear from it
-    if not config("PYTHONASYNCIODEBUG", cast=bool, default=False):
-        logging.getLogger("asyncio").setLevel(logging.WARNING)
-
-    # TODO httpcore, httpx, urlconnection, etc
 
 
 def configure_logger():
---
+++ structlog_config/stdlib_logging.py
@@ -0,0 +1,116 @@
+import logging
+import sys
+
+import structlog
+from decouple import config
+
+from .environments import is_production, is_staging
+
+
+def _get_log_level():
+    log_level = config("LOG_LEVEL", default="INFO", cast=str)
+    return logging.getLevelNamesMapping()[log_level.upper()]
+
+
+def reset_stdlib_logger(
+    logger_name: str, default_structlog_handler, level_override=None
+):
+    std_logger = logging.getLogger(logger_name)
+    std_logger.propagate = False
+    std_logger.handlers = []
+    std_logger.addHandler(default_structlog_handler)
+
+    if level_override:
+        std_logger.setLevel(level_override)
+
+
+def redirect_stdlib_loggers():
+    """
+    Redirect all standard logging module loggers to use the structlog configuration.
+
+    Inspired by: https://gist.github.com/nymous/f138c7f06062b7c43c060bf03759c29e
+    """
+    from structlog.stdlib import ProcessorFormatter
+
+    level = _get_log_level()
+
+    # Create a handler for the root logger
+    handler = logging.StreamHandler(sys.stdout)
+    handler.setLevel(level)
+
+    # TODO I don't understand why we can't use a processor stack as-is here. Need to investigate further.
+
+    # Use ProcessorFormatter to format log records using structlog processors
+    from .__init__ import PROCESSORS
+
+    formatter = ProcessorFormatter(
+        processors=[
+            # required to strip extra keys that the structlog stdlib bindings add in
+            structlog.stdlib.ProcessorFormatter.remove_processors_meta,
+            PROCESSORS[-1]
+            if not is_production() and not is_staging()
+            # don't use ORJSON here, as the stdlib formatter chain expects a str not a bytes
+            else structlog.processors.JSONRenderer(sort_keys=True),
+        ],
+        # processors unique to stdlib logging
+        foreign_pre_chain=[
+            # logger names are not supported when not using structlog.stdlib.LoggerFactory
+            # https://github.com/hynek/structlog/issues/254
+            structlog.stdlib.add_logger_name,
+            # omit the renderer so we can implement our own
+            *PROCESSORS[:-1],
+        ],
+    )
+    handler.setFormatter(formatter)
+
+    # Configure the root logger
+    root_logger = logging.getLogger()
+    root_logger.setLevel(level)
+    root_logger.handlers = [handler]  # Replace existing handlers with our handler
+
+    # Disable propagation to avoid duplicate logs
+    root_logger.propagate = True
+
+    # TODO there is a JSON-like format that can be used to configure loggers instead :/
+    std_logging_configuration = {
+        "httpcore": {},
+        "httpx": {
+            "levels": {
+                "INFO": "WARNING",
+            }
+        },
+        "azure.core.pipeline.policies.http_logging_policy": {
+            "levels": {
+                "INFO": "WARNING",
+            }
+        },
+    }
+    """
+    These loggers either:
+
+    1. Are way too chatty by default
+    2. Setup before our logging is initialized
+
+    This configuration allows us to easily override various loggers as we add additional complexity to the application
+    """
+
+    # now, let's handle some loggers that are probably already initialized with a handler
+    for logger_name, logger_config in std_logging_configuration.items():
+        reset_stdlib_logger(
+            logger_name,
+            handler,
+            logger_config.get("levels", {}).get(logging.getLevelName(level)),
+        )
+
+    # TODO do i need to setup exception overrides as well?
+    # https://gist.github.com/nymous/f138c7f06062b7c43c060bf03759c29e#file-custom_logging-py-L114-L128
+    if sys.excepthook != sys.__excepthook__:
+        logging.getLogger(__name__).warning("sys.excepthook has been overridden.")
+
+
+def silence_loud_loggers():
+    # unless we are explicitly debugging asyncio, I don't want to hear from it
+    if not config("PYTHONASYNCIODEBUG", cast=bool, default=False):
+        logging.getLogger("asyncio").setLevel(logging.WARNING)
+
+    # TODO httpcore, httpx, urlconnection, etc
