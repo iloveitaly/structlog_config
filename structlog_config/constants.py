0cceb7c541bd3f2d03ccf71392ee56ba5ae7f0bd
Michael Bianco <mike@mikebian.co>
2025-04-03 17:16:01 -0600

    fix: dynamically pull the LOG_LEVEL from env for testing

--- structlog_config/constants.py
+++ structlog_config/constants.py
@@ -2,7 +2,8 @@ import os
 
 from decouple import config
 
-LOG_LEVEL = config("LOG_LEVEL", default="INFO", cast=str).upper()
 PYTHON_LOG_PATH = config("PYTHON_LOG_PATH", default=None)
 PYTHONASYNCIODEBUG = config("PYTHONASYNCIODEBUG", default=False, cast=bool)
+
 NO_COLOR = "NO_COLOR" in os.environ
+"support NO_COLOR standard https://no-color.org"
